6d045d38c2188894fef2cfb90a965e6614132125
Yelshat Duskaliyev <yduskaliyev@semior.dev>
2025-04-06 00:20:23 +0300

    perf: unfuck ai comments

--- pkg/discovery/fileprovider/stdin.go
+++ pkg/discovery/fileprovider/stdin.go
@@ -19,24 +19,17 @@ type Stdin struct {
 }
 
 // Name returns the name of the provider.
-func (s *Stdin) Name() string {
-	return "stdin"
-}
+func (*Stdin) Name() string { return "stdin" }
 
 // Events sends a single event when the provider is created.
 // Since stdin can only be read once, this provider will only emit one event.
 func (s *Stdin) Events(ctx context.Context) <-chan string {
 	res := make(chan string, 1)
-	
-	// Send an event immediately, as we'll read from stdin only once
-	res <- s.Name()
-	
-	// We don't need to monitor stdin for changes
+	res <- s.Name() // send an event immediately, as we'll read from stdin only once
 	go func() {
 		<-ctx.Done()
 		close(res)
 	}()
-	
 	return res
 }
 
@@ -56,11 +49,10 @@ func (s *Stdin) State(ctx context.Context) (*discovery.State, error) {
 		return nil, fmt.Errorf("unsupported version: %s", cfg.Version)
 	}
 
-	// Reuse the existing implementation to process the config
 	slog.DebugContext(ctx, "parsed configuration from stdin")
 
-	file := &File{} // Create a temporary File provider without file watching capabilities
-	
+	file := &File{}
+
 	upstreams, err := file.upstreams(ctx, cfg)
 	if err != nil {
 		return nil, fmt.Errorf("get upstreams: %w", err)
